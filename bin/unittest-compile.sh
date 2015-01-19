9b426c80e781d912185570e2f335996bb5288e7f
Jason Pyeron <jpyeron@pdinc.us>
2015-01-19 15:12:51 -0500

    moved to Makefile for unit test build, also supporting gcovr reports for Jenkins

--- bin/unittest-compile.sh
+++ bin/unittest-compile.sh
@@ -1,46 +1,6 @@
 #!/bin/bash -x
 DIR="$(dirname "$0")/../"
 cd "$DIR"/src/unit-tests/
-
-g++ unittesting.cpp                                         \
-    -DCS_UNITTESTING                                        \
-    -fprofile-arcs -ftest-coverage                          \
-                                                            \
-    ../Common/Crc.c                                         \
-    ../Common/Endian.c                                      \
-    ../Common/Password.c                                    \
-    ../Common/strcpys.c                                     \
-    ../Common/util/unicode/ConvertUTF.c                     \
-    ../Common/util/unicode/strcmpw.c                        \
-                                                            \
-    faux/windows/CloseHandle.cpp                            \
-    faux/windows/CreateFile.cpp                             \
-    faux/windows/DeviceIoControl.cpp                        \
-    faux/windows/EnableWindow.cpp                           \
-    faux/windows/FlushFileBuffers.cpp                       \
-    faux/windows/GetFileSize.cpp                            \
-    faux/windows/GetFileTime.cpp                            \
-    faux/windows/GetLastError.cpp                           \
-    faux/windows/GetWindowText.cpp                          \
-    faux/windows/GetWindowTextLength.cpp                    \
-    faux/windows/SetFilePointer.cpp                         \
-    faux/windows/SetFileTime.cpp                            \
-    faux/windows/SetLastError.cpp                           \
-    faux/windows/MessageBox.c                               \
-    faux/ciphershed/wip.cpp                                 \
-                                                            \
-        -I .                                                \
-        -I ../../var/opt/cpptest-code/cpptest/src/          \
-../../var/opt/cpptest-code/cpptest/src/source.cpp           \
-../../var/opt/cpptest-code/cpptest/src/suite.cpp            \
-../../var/opt/cpptest-code/cpptest/src/textoutput.cpp       \
-../../var/opt/cpptest-code/cpptest/src/time.cpp             \
-../../var/opt/cpptest-code/cpptest/src/utils.cpp            \
-../../var/opt/cpptest-code/cpptest/src/collectoroutput.cpp  \
-../../var/opt/cpptest-code/cpptest/src/compileroutput.cpp   \
-../../var/opt/cpptest-code/cpptest/src/htmloutput.cpp       \
-../../var/opt/cpptest-code/cpptest/src/missing.cpp          \
--o unittesting
-
+make unittests
 exit $?
 
--- bin/unittest.sh
+++ bin/unittest.sh
@@ -1,12 +1,4 @@
 #!/bin/bash -x
 DIR="$(dirname "$0")/../"
 cd "$DIR"/src/unit-tests/
-
-../../bin/unittest-compile.sh && \
-set +x && \
-./unittesting || exit $?
-
-(
- find ../ -iname '*.cpp' -o -iname '*.c' -o -iname '*.h' | grep -v ^../unit-tests/ | while read line; do echo -e "File '$line'\nLines executed:0.00% of -1\n"; done
- for i in *.gcda; do gcov `basename $i .gcda`; done
-) | ../../bin/unittest-coverage-parse.pl 
+make coverage.xml
