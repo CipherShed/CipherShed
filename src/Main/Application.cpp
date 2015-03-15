CipherShed/CipherShed

commit 3d689ef6c8e21f6c2d4a1afcb5891192fd19bfa2

Error fix regarding wxStandardPaths

src/Main/Application.cpp
@@ -38,7 +38,6 @@ namespace CipherShed
 
 	FilePath Application::GetConfigFilePath (const wxString &configFileName, bool createConfigDir)
 	{
-		wxStandardPaths stdPaths;
 		DirectoryPath configDir;
 		
 		if (!Core->IsInPortableMode())
@@ -48,6 +47,7 @@ namespace CipherShed
 			configPath.Normalize();
 			configDir = wstring (configPath.GetFullPath());
 #else
+			wxStandardPaths& stdPaths = wxStandardPaths::Get();
 			configDir = wstring (stdPaths.GetUserDataDir());
 #endif
 		}

commit ad4356854bd30cd0f3f8f23db5d81c10d6758bd1

fixed wx 3.0 vs 2.8 issue, introduced in 3d689ef6 Error fix regarding wxStandardPaths
* Get returns a wxStandardPathsBase, not wxStandardPaths
* jenkins build failure: https://jenkins.ciphershed.client.pdinc.us/job/ciphershed-0.3.7.0-dev-64bit-centos6-application/5/
** Compiling Application.cpp
** TextUserInterface.h: In static member function ‘static CipherShed::FilePath CipherShed::Application::GetConfigFilePath(const wxString&, bool)’:
** TextUserInterface.h:108: error: invalid initialization of reference of type ‘wxStandardPaths&’ from expression of type ‘wxStandardPathsBase’
** make[1]: *** [Application.o] Error 1
** make: *** [all] Error 2

src/Main/Application.cpp
@@ -47,7 +47,7 @@ namespace CipherShed
 			configPath.Normalize();
 			configDir = wstring (configPath.GetFullPath());
 #else
-			wxStandardPaths& stdPaths = wxStandardPaths::Get();
+			wxStandardPathsBase& stdPaths = wxStandardPaths::Get();
 			configDir = wstring (stdPaths.GetUserDataDir());
 #endif
 		}