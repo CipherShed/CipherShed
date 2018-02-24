c329ba5f6607517e030d445156b36b41a3f66c12
Kyle Marek <psppsn96@gmail.com>
2018-02-24 05:13:35 -0500

    Workaround ld+objcopy issue regarding section flags
    objcopy is refusing to copy the .eos section because it doesn't have
    a flag that marks this as appropriate (see objdump -h output on
    boot.o without this patch applied). ld sets these flags based on the
    input sections, and doesn't appear to provide a *generic* way to set
    these flags to the appropriate target-specific values.
    
    So... working around by providing an empty section to copy flags from
    (in the form of a compiled empty assembly file).

---
+++ src/main/eos.s
@@ -0,0 +1,11 @@
+// TODO: Better solution?
+//
+// See: ld info PHDRS:
+//    The linker will normally set the segment flags based on the sections
+// which comprise the segment.  You may use the 'FLAGS' keyword to
+// explicitly specify the segment flags.  The value of FLAGS must be an
+// integer.  It is used to set the 'p_flags' field of the program header.
+//
+// Are the flag values generic or specific to the executable format
+// being linked?
+.section .eos
