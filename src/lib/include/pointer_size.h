3e5163ac1285226a3fb7b4dc39d997236a02ec81
Kyle Marek <psppsn96@gmail.com>
2018-05-06 19:23:45 -0400

    Add more useful pointer-size-related macros

--- src/lib/include/pointer_size.h
+++ src/lib/include/pointer_size.h
@@ -12,11 +12,17 @@
 	#define call_c calll
 	#define ret_c retl
 	#define sp_c %esp
+	#define push_c pushl
+	#define pop_c popl
+	#define ptr_bytes 4
 #elif defined POINTER16
 	#define jmp_c jmpw
 	#define call_c callw
 	#define ret_c retw
 	#define sp_c %sp
+	#define push_c pushw
+	#define pop_c popw
+	#define ptr_bytes 2
 #else
 	#error "Can't determine pointer size."
 #endif
