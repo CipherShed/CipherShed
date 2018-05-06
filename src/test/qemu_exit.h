61d1aaccbe44d05435c05b3fd79199d99709a976
Kyle Marek <psppsn96@gmail.com>
2018-05-06 18:39:51 -0400

    Document qemu_exit()'s exit code
    I realized that it is, otherwise, unclear why the two numbers used are
    different in 0aab08032f738cac17ce9a3f362457a1b8b5a0fa.

--- src/test/qemu_exit.h
+++ src/test/qemu_exit.h
@@ -1,3 +1,11 @@
+// exit qemu with a specific exit code
+//
+// The exit code is : (exit_code << 1) | 1
+//
+// See: qemu-2.12.0's hw/misc/debug_exit.c:29
+//     exit((val << 1) | 1);
+// See: https://git.qemu.org/?p=qemu.git;a=blob;f=hw/misc/debugexit.c;h=84fa1a5b9d550a298283924d2710b5a65f887558;hb=4743c23509a51bd4ee85cc272287a41917d1be35#l29
+
 //__attribute__((noreturn))
 __attribute__((stdcall))
 __attribute__((always_inline))
