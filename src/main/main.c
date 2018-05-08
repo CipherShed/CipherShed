382447f43f6e750a6f998b49b1653cbb1ab3a732
Kyle Marek <psppsn96@gmail.com>
2018-05-07 20:04:47 -0400

    Add basic example of reading data from drive

--- src/main/main.c
+++ src/main/main.c
@@ -21,12 +21,41 @@ inline static void print(char *string)
 	}
 }
 
+__attribute__((stdcall))
+inline static void read_sector_2(uint8_t drive, char* buf)
+{
+	asm volatile
+	(R"ASM(
+	mov $0, %%ax
+	mov %%ax, %%es # destination segment
+	mov %[buf], %%bx # destination
+
+	mov $2, %%ah # function
+
+	mov %[drive], %%dl # drive
+	mov $2, %%cx # (cylinder << 6) + sector
+	mov $0, %%dh # head
+	mov $1, %%al # sector count
+
+	int $0x13 # fixed disk services
+)ASM"
+		: /* no output */
+		:
+		  [drive] "m" (drive),
+		  [buf] "m" (buf)
+		: "ax", "bx", "cx", "dx"
+	);
+}
+
 __attribute__((section(".text.main")))
 __attribute__((noreturn))
 void cmain(uint16_t boot_drive) asm("main");
 void cmain(uint16_t boot_drive)
 {
-    print("Hello, World!");
+	char buf[512];
+
+	read_sector_2(boot_drive, buf);
+	print(buf);
 
     halt();
 }
