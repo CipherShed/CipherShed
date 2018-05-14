50cd68c6ae538086c865622fec69f4871219f8e9
Kyle Marek <psppsn96@gmail.com>
2018-05-13 22:58:20 -0400

    Break long command into multiple lines
    ... for diff legibility in editing this command.

--- src/test/Makefile.mk
+++ src/test/Makefile.mk
@@ -3,7 +3,17 @@ test: test_run
 
 .PHONY: test_run
 test_run: ${bindir}/kernel
-	$(QEMU_SYSTEM_I386) -display none -nodefaults -nodefconfig -no-user-config -m 1M -device isa-debug-exit -chardev file,id=ttyS0,path=/dev/fd/2 -device isa-serial,chardev=ttyS0 -kernel $<; test $$? -eq 3
+	$(QEMU_SYSTEM_I386) \
+		-display none \
+		-nodefaults \
+		-nodefconfig \
+		-no-user-config \
+		-m 1M \
+		-device isa-debug-exit \
+		-chardev file,id=ttyS0,path=/dev/fd/2 \
+		-device isa-serial,chardev=ttyS0 \
+		-kernel $<; \
+		test $$? -eq 3
 
 .PHONY:
 test_build: ${bindir}/kernel
