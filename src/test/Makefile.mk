01732136fd25f3a3fa70225cf59e665ff481e823
Kyle Marek <psppsn96@gmail.com>
2018-05-13 23:57:00 -0400

    Add drives to unit test
    In preparation of I/O development.
    
    Note on the "test_run: bindir:=${bindir}" line: While Make immediately
    expands variables for targets and dependencies, it does not expand
    variables in recipes until the recipes are called. Because of this,
    "${bindir}" in the kernel recipe was expanding to a path derived from
    one of the "${MDEPS}" Makefiles, rather than the current Makefile.
    
    By using ":=" to re-set the the variable for that target, I'm
    effectively pre-expanding it at target declaration, so its value at
    run-time of the recipe matches the value used to define the target in
    the first place.

--- src/test/Makefile.mk
+++ src/test/Makefile.mk
@@ -2,7 +2,8 @@
 test: test_run
 
 .PHONY: test_run
-test_run: ${bindir}/kernel
+test_run: bindir:=${bindir}
+test_run: ${bindir}/kernel ${bindir}/floppy00.img ${bindir}/drive80.img
 	$(QEMU_SYSTEM_I386) \
 		-display none \
 		-nodefaults \
@@ -12,7 +13,10 @@ test_run: ${bindir}/kernel
 		-device isa-debug-exit \
 		-chardev file,id=ttyS0,path=/dev/fd/2 \
 		-device isa-serial,chardev=ttyS0 \
-		-kernel $<; \
+		-kernel ${bindir}/kernel \
+		-drive file=${bindir}/floppy00.img,format=raw,if=floppy \
+		-drive file=${bindir}/drive80.img,format=raw \
+		;
 		test $$? -eq 3
 
 .PHONY:
@@ -21,5 +25,11 @@ test_build: ${bindir}/kernel
 ${bindir}/kernel: LDFLAGS := ${LDFLAGS} -Wl,--script=${subdir}/kernel-unisection.ld
 ${bindir}/kernel: ${OBJS} ${lib}
 	$(recipe_link)
+
+${bindir}/floppy00.img:
+	truncate --size=$$((2880*512)) $@
+
+${bindir}/drive80.img:
+	truncate --size=$$((2880*512)) $@
 
 -include ${MDEPS}
