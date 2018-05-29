8d4cb6d478e60fa1d801f31ac1ed8a76f8b737b0
Kyle Marek <psppsn96@gmail.com>
2018-05-28 22:41:39 -0400

    Address patch dependency
    I overlooked a commit where Stefan split his indicator patch into two
    patches [1]. His indicator patch depends on the other patch, so history
    extraction fails.
    
    To address this while only tracking the history of the indicator patch,
    the history extraction script creates this part of the history in a
    second branch onto a commit that fills the dependency.
    
    Rebasing the second branch after the dependency-filling commit onto the
    first branch will create a branch tracking the development of the
    indicator patch independently of the dependent patch.
    
    [1]:
    See: https://github.com/stefansundin/truecrypt.deb/commit/0569ec52dbec589af1cb5e9609b4fa9a1afa7994

--- bin/indicator.stefansundin.history.sh
+++ bin/indicator.stefansundin.history.sh
@@ -3,24 +3,65 @@
 basecommit=e0b39ef434fdb0a225f2e9dd71bdd677c73e3231
 repo=https://github.com/stefansundin/truecrypt.deb.git
 remote=stefansundin
-branch=indicator.stefansundin
+branch1=indicator.stefansundin.step1
+branch2=indicator.stefansundin.step2
 patch=patches/truecrypt-7.1a-indicator.patch
 patch_source=refs/remotes/$remote/master
+
+prepatch=patches/truecrypt-7.1a-build-fixes.patch
+prepatch_source=0569ec52dbec589af1cb5e9609b4fa9a1afa7994
 
 ## Clean up (allow redoing)
 #git reset --hard HEAD
 #git clean -dxf
 #git checkout refs/remotes/origin/master
-#git branch -D "$branch"
+#git branch -D "$branch1"
+#git branch -D "$branch2"
 #git remote remove "$remote"
 
 git remote add "$remote" "$repo"
 git fetch "$remote"
-git checkout -b "$branch" "$basecommit"
-commits="$(git log --reverse --format=format:%H "$patch_source" -- "$patch")"
+commits1="$(git log --reverse --format=format:%H "$prepatch_source"^1 -- patches/truecrypt-7.1a-indicator.patch)"
+commits2="$(git log --reverse --format=format:%H "$prepatch_source"^'!' refs/remotes/stefansundin/master -- patches/truecrypt-7.1a-indicator.patch)"
 
-for commit in $commits; do
+# Revisions before patches/truecrypt-7.1a-build-fixes.patch dependency
+git checkout -b "$branch1" "$basecommit"
+for commit in $commits1; do
 	git checkout "$basecommit" .
+	git show "$commit:$patch" | ( cd src/ && git apply -; )
+	git add src/
+
+	author="$(git show -s --format="%an <%ae>" "$commit")"
+	date="$(git show -s --format="%ad" "$commit")"
+	message="$(git show -s --format="%B" "$commit")"
+
+	message="$message
+
+(applied $patch at $commit in $repo)"
+
+	git commit -q --no-gpg-sign -m "$message" --allow-empty --author="$author" --date="$date"
+done
+
+# Address patches/truecrypt-7.1a-build-fixes.patch dependency
+git checkout -b "$branch2"
+git checkout "$basecommit" .
+git show "$prepatch_source:$prepatch" | ( cd src/ && git apply -; )
+git add src/
+
+author="$(git show -s --format="%an <%ae>" "$prepatch_source")"
+date="$(git show -s --format="%ad" "$prepatch_source")"
+message="$(git show -s --format="%B" "$prepatch_source")"
+
+message="$message
+
+(applied $prepatch at $prepatch_source in $repo)"
+
+git commit -q --no-gpg-sign -m "$message" --allow-empty --author="$author" --date="$date"
+
+# Revisions after patches/truecrypt-7.1a-build-fixes.patch dependency
+for commit in $commits2; do
+	git checkout "$basecommit" .
+	git show "$prepatch_source:$prepatch" | ( cd src/ && git apply -; )
 	git show "$commit:$patch" | ( cd src/ && git apply -; )
 	git add src/
 
